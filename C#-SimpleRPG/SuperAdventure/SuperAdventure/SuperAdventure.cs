20b89d932848951962d98f87c3a13fb8b3f41bd4
Zhouyi Yu <zhouyi1@ualberta.ca>
2024-09-11 18:41:00 -0600

    u

---
+++ C#-SimpleRPG/SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+            //Initialization numbers
+
+            //Location location = new Location(1, "Home", "Home, Sweet Home~~");
+            
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            //Write them on the UI
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();   
+        }
+    }
+}
